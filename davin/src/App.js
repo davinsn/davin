1a4a532d20e46043b9c41d0854b2b114d7d0268b
davinsn <s4021650@student.rmit.edu.au>
2025-02-25 17:39:47 +1100

    changes to information in each page (added more info)

--- davin/src/App.js
+++ davin/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link, Element } from "react-scroll";
+import { Element } from "react-scroll";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import About from "./components/About";
--- davin/src/components/About.js
+++ davin/src/components/About.js
@@ -8,6 +8,21 @@ const About = () => {
       <p className="about-text">
         I am a full-time student from Malaysia studying Computer Science at RMIT University.
       </p>
+      <p className="about-text">
+        I am passionate about technology and I am always eager to learn new things.
+        I have taken part in various group projects and exams that have helped me to develop my skills.
+        </p>
+        <h2 className="about-title">Here's some technology I have worked with:</h2>
+        <ul className="about-list">
+          <li className="about-list-item">JavaScript</li>
+          <li className="about-list-item">React</li>
+          <li className="about-list-item">Node.js</li>
+          <li className="about-list-item">Python</li>
+          <li className="about-list-item">HTML & CSS</li>
+          <li className="about-list-item">Java</li>
+          <li className="about-list-item">C++</li>
+          <li className="about-list-item">GraphQL</li>
+        </ul>
     </div>
   );
 };
